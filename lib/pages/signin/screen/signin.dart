01abd5bd067b67400e27d5219b6e306bf36f6517
Herry Prasetyo <herry081288@gmail.com>
2022-01-23 13:43:50 +0700

    signInDesign

--- lib/pages/signin/screen/signin.dart
+++ lib/pages/signin/screen/signin.dart
@@ -9,6 +9,19 @@ class HerryCommerceSignInScreen extends StatefulWidget {
 }
 
 class _HerryCommerceSignInScreenState extends State<HerryCommerceSignInScreen> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  bool _obscureText = true;
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController(text: '');
+    _passwordController = TextEditingController(text: '');
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +38,15 @@ class _HerryCommerceSignInScreenState extends State<HerryCommerceSignInScreen> {
             const SizedBox(
               height: Const.space25,
             ),
-            
+            CustomTextFormField(
+              controller: _emailController,
+              hintText: AppLocalizations.of(context)!.email_address,
+              keyboardType: TextInputType.emailAddress,
+              prefixIcon: const Icon()
+            ),
+            SizedBox(
+              height: Const.space15,
+            ),
           ],
         ),
       ),
--- lib/pages/signin/sign_in_page.dart
+++ lib/pages/signin/sign_in_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:uikit/helpers/constans.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:uikit/pages/widget/customtextfield.dart';
+
+//ui
 part './screen/signin.dart';
 part '../widget/buildheader.dart';
